33fb6b8bd3858ea8ffd6165ec162d6996ac8e35e
nora youssefi <nora.youssefi@etu.uae.ac.ma>
2022-10-13 14:49:31 +0200

    new commit

--- js/resume.js
+++ js/resume.js
@@ -17,20 +17,17 @@ function alterner(id){
     if(id=='stage'){
         details('stage');
         document.getElementById('imo').style.display = 'none';
-        document.getElementById('depart').style.display = 'none';
-       
+        document.getElementById('depart').style.display = 'none';     
     }
     else if(id=="imo"){
         details('imo');
         document.getElementById('stage').style.display = 'none';
-        document.getElementById('depart').style.display = 'none';
-      
+        document.getElementById('depart').style.display = 'none';     
     }
     else {
         details('depart');
         document.getElementById('stage').style.display = 'none';
         document.getElementById('imo').style.display = 'none';
-      
     }
 }
  
@@ -68,3 +65,84 @@ function fn(e) {
         tooltip[i].style.top = e.pageY + 'px';
     }
 }
+
+//question 7
+function  evaluerConnaissance(){
+    let competences = document.querySelectorAll(".rating");
+    try {
+        for(let i=0; i < competences.length; i++){
+            let r = competences[i].getAttribute("rating");
+
+            if(r!=null){
+
+                let starts = '<span style="color:#eeee6a; " >' 
+
+                for(let j=0; j< r; j++){
+                    starts+="★";
+                }
+                starts += '</span>';
+
+                starts += '<span>';
+                for(let j=r; j<5; j++){
+                    starts+="★";
+                }
+                starts += '</span>';
+
+                competences[i].innerHTML += starts;
+
+            }
+
+        }
+        console.log("error")
+    } catch (error) {
+        console.log(error)
+    }
+}
+evaluerConnaissance();
+
+//question 8
+mesCompetence ={
+    "JAVA/JEE" : 4 ,
+    "SpringBoot" : 4,
+    "Python" : 4,
+    "Django" : 3,
+    "Angular" : 3 ,
+    "PHP/Laravel" : 3,
+    "MySQL" : 4 ,
+    "MongoDB" : 4,
+    "PostgreSQL" : 3 ,
+    "UML" : 5,
+    "Merise" : 3,
+    "SCRUM" : 4
+    
+}
+
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: Object.keys(mesCompetence),
+    datasets: [{
+      label: 'Competences',
+      data: Object.values(mesCompetence),
+      backgroundColor: 'rgba(153, 132, 128)',
+    }]
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        display: true,
+        barPercentage: 1.2, 
+     }],
+      yAxes: [{
+        ticks: {
+          max: 5,
+          beginAtZero:true,
+          stepSize: 1
+        },
+        stacked: true
+
+      }]
+    }
+  }
+});
